b8b2e48f542a7542732ff1988311d8be5d598089
yaahsin <brillanteoboe@gmail.com>
2022-05-17 23:06:06 +0800

    fix methodoverride

--- app.js
+++ app.js
@@ -22,11 +22,12 @@ app.use(express.static('public'))
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// 打算使用put及delete，取代本來http的語法
+// 注意要放在route前面，不然就不能override了
+app.use(methodOverride('_method'))
+
 // 將 request 導入路由器
 app.use(routes)
-
-// 打算使用put及delete，取代本來http的語法
-app.use(methodOverride('_method'))
 
 // start and listen on the Express server
 app.listen(port, () => {
