47368727ce477928790f69b404c862ea5a82839b
yaahsin <brillanteoboe@gmail.com>
2022-06-05 20:20:15 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ const router = express.Router()
 
 // 引入 home 模組程式碼
 const home = require('./modules/home')
-// 引入 restaurants 模組程式碼
+// 引入 restaurants and users 模組程式碼
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 // 將網址結構符合字串開頭的request, 導向對應模組
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 
 // 匯出路由器
---
+++ routes/modules/users.js
@@ -0,0 +1,11 @@
+// routes/modules/users.js
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+
+module.exports = router
