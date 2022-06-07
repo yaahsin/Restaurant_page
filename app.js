94692ef7450dec6b0389fed514517ff15c6fe762
yaahsin <brillanteoboe@gmail.com>
2022-06-07 22:33:17 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 // require packages used handlebars body-parser in the project
 const express = require('express')
 const session = require('express-session')
+const bodyParser = require('body-parser')
 const port = 3000
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 // 引用路由器
 const routes = require('./routes')
@@ -37,10 +38,13 @@ app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+app.use(flash())
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -12,7 +12,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
@@ -22,12 +23,30 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   // 檢查使用者是否已經註冊
   User.findOne({ email }).then(user => { //findOne是找{物件}
     // 如果已經註冊：退回原本畫面
     if (user) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       console.log('User already exists.')
       res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -49,6 +68,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
