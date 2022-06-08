1e7736c1ffa9e6841bf33a56f1375d0d93f4bde6
yaahsin <brillanteoboe@gmail.com>
2022-06-08 23:18:55 +0800

    feat: modify todoSeeder_v1

--- models/seeds/RestaurantSeeder.js
+++ models/seeds/RestaurantSeeder.js
@@ -1,21 +1,60 @@
 // models/seeds
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const RestaurantList = require('../Restaurant')
+const User = require('../user')
 const db = require('../../config/mongoose')
 
 // 取得上上一層目錄的json檔資料
 const restaurantList = require('../../restaurant.json').results
+const SEED_USER = [{
+  email: 'user1@example.com',
+  password: '12345678'
+},
+{
+  email: 'user2@example.com',
+  password: '12345678'
+}]
 
 db.once('open', () => {
   console.log('running restaurantSeeder script...')
-
-  // 將檔案restaurantList.json以Restaurant的schema設定create進db裡面
-  // 所以必須先在models裡面, 建立一個Restaurant.js設定schema
-  // 檔案非按照順序排進去, 因為是非同步運行, 先到者先進資料庫
-  RestaurantList.create(restaurantList)
-    .then(() => {
-      console.log('restaurantSeeder done!')
-      db.close() // 不設定關掉的話, 不知道會怎樣子
+  const saltRounds = 10
+  SEED_USER.forEach(item => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      bcrypt.hash(item.password, salt, (err, hash) => {
+        // Store hash in your password DB.
+        return User.create({
+          email: item.email,
+          password: hash
+        })
+          .then(user => {
+            const userId = user._id
+            if (user.email === 'user1@example.com') {
+              for (let i = 0; i < 3; i++) {
+                let { name, name_en, category, image, location, phone, google_map, rating, description, } = restaurantList[i]
+                RestaurantList.create({
+                  name, name_en, category, image, location, phone, google_map,
+                  rating: Number(rating),
+                  description, userId
+                })
+              }
+            } else {
+              for (let i = 3; i < 6; i++) {
+                let { name, name_en, category, image, location, phone, google_map, rating, description, } = restaurantList[i]
+                RestaurantList.create({
+                  name, name_en, category, image, location, phone, google_map,
+                  rating: Number(rating),
+                  description, userId
+                })
+              }
+            }
+          })
+          .catch(err => console.log(err))
+      })
     })
-    .catch(err => console.log(err))
+  })
+  console.log('restaurantSeeder done!')
 })
