397b7b1af27301f8198aaa4e591fefa29195e22a
yaahsin <brillanteoboe@gmail.com>
2022-06-09 21:18:25 +0800

    feat: modify todoSeeder_v2(Pormise.all)

--- models/seeds/RestaurantSeeder.js
+++ models/seeds/RestaurantSeeder.js
@@ -1,60 +1,49 @@
 // models/seeds
 const bcrypt = require('bcryptjs')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const RestaurantList = require('../Restaurant')
+const Restaurant = require('../Restaurant')
 const User = require('../user')
 const db = require('../../config/mongoose')
 
 // 取得上上一層目錄的json檔資料
 const restaurantList = require('../../restaurant.json').results
+
 const SEED_USER = [{
   email: 'user1@example.com',
-  password: '12345678'
+  password: '12345678',
+  InitialList: [1, 2, 3]
 },
 {
   email: 'user2@example.com',
-  password: '12345678'
+  password: '12345678',
+  InitialList: [4, 5, 6]
 }]
 
 db.once('open', () => {
-  console.log('running restaurantSeeder script...')
-  const saltRounds = 10
-  SEED_USER.forEach(item => {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-      bcrypt.hash(item.password, salt, (err, hash) => {
-        // Store hash in your password DB.
-        return User.create({
-          email: item.email,
-          password: hash
-        })
-          .then(user => {
-            const userId = user._id
-            if (user.email === 'user1@example.com') {
-              for (let i = 0; i < 3; i++) {
-                let { name, name_en, category, image, location, phone, google_map, rating, description, } = restaurantList[i]
-                RestaurantList.create({
-                  name, name_en, category, image, location, phone, google_map,
-                  rating: Number(rating),
-                  description, userId
-                })
-              }
-            } else {
-              for (let i = 3; i < 6; i++) {
-                let { name, name_en, category, image, location, phone, google_map, rating, description, } = restaurantList[i]
-                RestaurantList.create({
-                  name, name_en, category, image, location, phone, google_map,
-                  rating: Number(rating),
-                  description, userId
-                })
-              }
-            }
-          })
-          .catch(err => console.log(err))
+  console.log('running')
+  // 待全部做完 (回傳一個新陣列)
+  Promise.all(SEED_USER.map(SEED_USER =>
+    // 前處理密碼
+    bcrypt.genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER.password, salt))
+      .then(hash => User.create({ name: SEED_USER.name, email: SEED_USER.email, password: hash }))
+      .then(user => {
+        const userId = user._id
+        // 抓出目標餐廳清單
+        const restaurants = restaurantList.filter(restaurant => SEED_USER.InitialList.includes(restaurant.id))
+        // 寫入userId標記
+        restaurants.forEach(restaurant => restaurant.userId = userId)
+        // 將其建立進個人餐廳清單
+        return Restaurant.create(restaurants)
       })
+  ))
+    .then(() => {
+      console.log('done')
+      process.exit()
     })
-  })
-  console.log('restaurantSeeder done!')
+    .catch(err => console.log(err))
 })
